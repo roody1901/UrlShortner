d35312b0cfbe74ddeb46823454e287f7ff9174f6
Vibhanshu Pandey <vibhanshu.p19@gmail.com>
2025-01-31 16:31:08 +0530

    fix - urlservice logic adding

--- src/main/java/com/example/urlshortner/URLShortner/service/UrlServiceImplement.java
+++ src/main/java/com/example/urlshortner/URLShortner/service/UrlServiceImplement.java
@@ -5,6 +5,8 @@ import com.example.urlshortner.URLShortner.repository.UrlRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 
 @Service
 public class UrlServiceImplement implements UrlService{
@@ -16,13 +18,37 @@ public class UrlServiceImplement implements UrlService{
         this.urlRepository = urlRepository;
     }
 
+    private final static String  elements = "abcedefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
     @Override
     public String generateShortUrl(URLDetails urlData) {
-        return null;
+        long uniqueRandom = urlData.getId();
+        String shorturl =  convertToShort(uniqueRandom);
+        urlData.setShortUrl(shorturl);
+        urlRepository.save(urlData);
+        return "crunchyurl.com/"+shorturl;
     }
 
     @Override
     public String getLongUrl(String shortUrl) {
-        return null;
+      shortUrl =  shortUrl.substring("crunchyurl/".length());
+      long id =0;
+//       for(int i = 0;i<shortUrl.length();i++){
+//           id  = id * 62 + (shortUrl.charAt(i));
+//       }
+        return "No urll found";
+    }
+
+    private String convertToShort(long n){
+        StringBuilder sb = new StringBuilder();
+        while(n != 0){
+            sb.insert(0,elements.charAt((int) (n%62)));
+            n = n/62;
+        }
+        while(sb.length()!=7){
+            sb.insert(0,'0');
+        }
+
+        return sb.toString();
     }
 }
